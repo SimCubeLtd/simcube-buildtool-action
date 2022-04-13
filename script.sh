e080fe8805c37946727fe7a3127379ad9fd4842d
prom3theu5 <dave@simcube.co.uk>
2022-04-13 23:59:29 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,11 +3,19 @@
 set -e
 
 BuildToolPath=${SIMCUBE_DOTNET_TOOL_PATH:-/home/prom3theu5/.dotnet/tools}
+zero=0;
 
 echo "SimCube.BuildTool"
+
 if bash -c "${BuildToolPath}/SimCube.BuildTool -t DotNet"; then
     # no op
-    echo "Successfully Completed"
+    exit_code=$?
+    if [[ $exit_code -eq $zero ]]; then
+        echo "Successfully Completed"
+    else
+        echo "ERROR: running '${BuildToolPath}/SimCube.BuildTool', exited with $exit_code"
+        exit $exit_code       
+    fi
 else
     exit_code=$?
     echo "ERROR: running '${BuildToolPath}/SimCube.BuildTool', exited with $exit_code"
