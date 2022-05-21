d4667e25b011545c8b1c528b1af26db378cd5b46
prom3theu5 <dave@simcube.co.uk>
2022-05-21 13:14:47 +0100

    Change tool binary
    Renamed toolcommand in latest package

--- script.sh
+++ script.sh
@@ -3,20 +3,21 @@
 set -e
 
 zero=0;
+toolbinary="simcubebuildtool"
 
 echo "SimCube.BuildTool"
 
-if bash -c "${SIMCUBE_DOTNET_TOOL_PATH}/SimCube.BuildTool -t DotNet"; then
+if bash -c "${SIMCUBE_DOTNET_TOOL_PATH}/${toolbinary} -t DotNet"; then
     # no op
     exit_code=$?
     if [[ $exit_code -eq $zero ]]; then
         echo "Successfully Completed"
     else
-        echo "ERROR: running '${SIMCUBE_DOTNET_TOOL_PATH}/SimCube.BuildTool', exited with $exit_code"
+        echo "ERROR: running '${SIMCUBE_DOTNET_TOOL_PATH}/${toolbinary}', exited with $exit_code"
         exit $exit_code       
     fi
 else
     exit_code=$?
-    echo "ERROR: running '${SIMCUBE_DOTNET_TOOL_PATH}/SimCube.BuildTool', exited with $exit_code"
+    echo "ERROR: running '${SIMCUBE_DOTNET_TOOL_PATH}/${toolbinary}', exited with $exit_code"
     exit $exit_code
 fi
